1e3e2189e7839b5a7ca43f67b0827a52d6deb992
MaksymProtsak <maksymprotsak@MaksymProtsaks-MacBook-Pro.local>
2025-09-27 15:16:32 +0300

    Finished lesson 2.

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ import logging
 
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 load_dotenv()
@@ -16,7 +16,30 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Hello!")
+    await message.reply(f"Hello!\nYour ID: {message.from_user.id}\nYour Name: {message.from_user.first_name}")
+
+
+@dp.message(Command("help"))
+async def get_help(message: Message):
+    await message.answer("Це команда /help")
+
+
+@dp.message(F.text =="Як справи?")
+async def how_are_you(message: Message):
+    await message.answer("Ok!")
+
+
+@dp.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f"ID фото: {message.photo[-1].file_id}")
+
+
+@dp.message(Command("get_photo"))
+async def get_photo(message: Message):
+    await message.answer_photo(
+        photo="AgACAgIAAxkBAAImJmjX0w9A-JVwttPnKT_uMGHYHeK2AAK0BzIbB5rBSjgnaH5iwXIpAQADAgADeQADNgQ",
+        caption="Це твоє фото"
+    )
 
 
 async def main():
